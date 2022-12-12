f89bc044a7669f2ed7decd940090dec533ad0889
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2022-12-12 13:52:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (38.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> step(n+1,0);
+        // vector<int> step(n+1,0);
+        // if(n==1){
+        //     return 1;
+        // }
+        // step[1]=1;
+        // step[2]=2;
+        // for(int i=3;i<=n;i++){
+        //     step[i]=step[i-1]+step[i-2];
+        // }
+        // return step[n];
+        
+        //Space optimizations
         if(n==1){
             return 1;
         }
-        step[1]=1;
-        step[2]=2;
+        int second_last=1,last=2,curr=last;
         for(int i=3;i<=n;i++){
-            step[i]=step[i-1]+step[i-2];
+            curr=last+second_last;
+            second_last=last;
+            last=curr;
         }
-        return step[n];
+        return last;
     }
 };

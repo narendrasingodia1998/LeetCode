47eac8526b75183e6891361c103da9faa201030b
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-01-10 17:20:39 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (48.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,24 +11,18 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    bool recursive(TreeNode* p,TreeNode* q){
         //Base Condition
         if(p==NULL&&q==NULL){
             return true;
         }
-        if(p==NULL){
-            return false;
-        }
-        if(q==NULL){
+        if(p==NULL||q==NULL){
             return false;
         }
         //General Condition
-        if(p->val!=q->val){
-            return false;
-        }
-        if(!isSameTree(p->left,q->left)){
-            return false;
-        }
-        return isSameTree(p->right,q->right);
+        return p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return recursive(p,q);
     }
 };

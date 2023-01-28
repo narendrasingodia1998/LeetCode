c6504d9a55b0494aa2f207ad1d98fc2fe95839f9
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-01-29 00:59:23 +0530

    Time: 243 ms (71.45%), Space: 21.2 MB (71.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
         n=len(nums)
         left_prod=[1]*n
         right_prod=[1]*n
@@ -8,5 +9,17 @@ class Solution:
         for i in range(n-2,-1,-1):
             right_prod[i]=right_prod[i+1]*nums[i+1]
         res=[a*b for a,b in zip(left_prod,right_prod)]
+        return res
+        """
+        n=len(nums)
+        res=[1]*n
+        prefix=1
+        for i,val in enumerate(nums):
+            res[i]=prefix
+            prefix*=nums[i]
+        postfix=1
+        for i in range(n-1,-1,-1):
+            res[i]*=postfix
+            postfix*=nums[i]
         return res
             

ffdb0d6a23d9671e2c66bfd126c8646ff9af7e39
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-01-30 16:09:40 +0530

    Time: 96 ms (82.20%), Space: 13.9 MB (76.59%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,34 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #Brute Force
-        #Checking for column
-        for row in range(9):
-            check=[0]*10
-            for col in range(9):
-                if board[row][col] !=".":
-                    ele=ord(board[row][col])-ord("0")
-                    if check[ele]:
-                        print(row,col)
-                        return False
-                    check[ele]=1
-        #Checking for row
-        for col in range(9):
-            check=[0]*10
-            for row in range(9):
-                if board[row][col]!=".":
-                    ele=ord(board[row][col])-ord("0")
-                    if check[ele]:
-                        return False
-                    check[ele]=1
-        #Checking for 3*3 box
-        for i in range(3):
-            for j in range(3):
-                check=[0]*10
-                for row in range(3):
-                    for col in range(3):
-                        if board[i*3+row][j*3+col]!=".":
-                            ele=ord(board[i*3+row][j*3+col])-ord("0")
-                            if check[ele]:
-                                return False
-                            check[ele]=1
+        row=collections.defaultdict(set)
+        col=collections.defaultdict(set)
+        square=collections.defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]=='.':
+                    continue
+                if (board[i][j] in row[i] or
+                    board[i][j] in col[j] or
+                    board[i][j] in square[(i//3,j//3)]):
+                    return False
+                row[i].add(board[i][j])
+                col[j].add(board[i][j])
+                square[(i//3,j//3)].add(board[i][j])
         return True
+                

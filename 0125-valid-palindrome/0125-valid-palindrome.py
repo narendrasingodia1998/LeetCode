983d21962bafc656e5e7feed99f3c758653df52d
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-02-02 16:17:45 +0530

    Time: 61 ms (43.22%), Space: 14.6 MB (67.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,15 @@
 class Solution:
+    def alphanumeric(self,s):
+        return ('a'<=s<='z') or ('0'<=s<='9')
+    
     def isPalindrome(self, s: str) -> bool:
         s=s.lower()
         i,j,n=0,len(s)-1,len(s)
         while i<j:
             #setting i non_alphanumeric Character
-            while (i<n and 
-                  not ((s[i]>='a' and s[i]<='z') or 
-                    (s[i]>='0' and s[i]<='9'))):
+            while (i<n and not self.alphanumeric(s[i])):
                 i+=1
-            while (j>=0 and 
-                 not  ((s[j]>='a' and s[j]<='z') or 
-                    (s[j]>='0' and s[j]<='9'))):
+            while (j>=0 and not self.alphanumeric(s[j])):
                 j-=1
             if i<j and s[i]!=s[j]:
                 return False

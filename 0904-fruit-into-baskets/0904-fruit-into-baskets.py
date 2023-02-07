316d09ad7c061fbd83d54a460700ea094ad38d67
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-02-07 13:24:04 +0530

    Time: 863 ms (89.10%), Space: 20.6 MB (13.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,5 +1,5 @@
 class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
+    def method1(self,fruits):
         dict1=defaultdict(int)
         l=r=0
         n=len(fruits)
@@ -14,3 +14,19 @@ class Solution:
             ans=max(ans,r-l+1)
             r+=1
         return ans
+    
+    def method2(self,fruits):
+        count=defaultdict(int)
+        l=0
+        for r,tree in enumerate(fruits):
+            count[tree]+=1
+            if len(count)>2:
+                count[fruits[l]]-=1
+                if not count[fruits[l]]:
+                    del count[fruits[l]]
+                l+=1
+        return r-l+1
+    
+    def totalFruit(self, fruits: List[int]) -> int:
+        #return self.method1(fruits)
+        return self.method2(fruits)

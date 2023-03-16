b69ce271981a51ac06770c0bdf72a893658428f2
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-03-16 21:27:02 +0530

    Time: 538 ms (5.04%), Space: 88.8 MB (23.52%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -5,19 +5,6 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def solve(self,inorder,postorder):
-        #Base Condition
-        if not inorder:
-            return
-        val=postorder[-1]
-        index=0
-        while index<len(inorder) and inorder[index]!=val:
-            index+=1
-        root=TreeNode(postorder[-1])
-        root.left=self.solve(inorder[0:index],postorder[0:index])
-        root.right=self.solve(inorder[index+1:],postorder[index:-1])
-        return root
-    
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         #Base Condition
         if not inorder:
@@ -27,6 +14,6 @@ class Solution:
         while index<len(inorder) and inorder[index]!=val:
             index+=1
         root=TreeNode(postorder[-1])
-        root.left=self.solve(inorder[0:index],postorder[0:index])
-        root.right=self.solve(inorder[index+1:],postorder[index:-1])
+        root.left=self.buildTree(inorder[0:index],postorder[0:index])
+        root.right=self.buildTree(inorder[index+1:],postorder[index:-1])
         return root

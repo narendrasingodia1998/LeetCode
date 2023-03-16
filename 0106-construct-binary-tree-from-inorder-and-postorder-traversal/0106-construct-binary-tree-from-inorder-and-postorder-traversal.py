4a35a865c56e212800583daea3aaf48502e00a73
narendrasingodia1998 <30290272+narendrasingodia1998@users.noreply.github.com>
2023-03-16 21:40:50 +0530

    Time: 271 ms (12.58%), Space: 19.1 MB (67.64%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,9 +6,11 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        """
+        #Time Complexity is O(n*n)
         #Base Condition
         if not inorder:
-            return
+            return None
         val=postorder[-1]
         index=0
         while index<len(inorder) and inorder[index]!=val:
@@ -17,3 +19,21 @@ class Solution:
         root.left=self.buildTree(inorder[0:index],postorder[0:index])
         root.right=self.buildTree(inorder[index+1:],postorder[index:-1])
         return root
+        """
+        
+        def solve(in_start,in_end,post_start,post_end):
+            #Base Condition
+            if in_start>in_end:
+                return None
+            val=postorder[post_end]
+            index=in_start
+            while inorder[index]!=val:
+                index+=1
+            root=TreeNode(val)
+            length=(index-1)-in_start+1
+            root.left=solve(in_start,index-1,post_start,post_start+length-1)
+            root.right=solve(index+1,in_end,post_start+length,post_end-1)
+            return root
+        
+        n=len(inorder)
+        return solve(0,n-1,0,n-1)
